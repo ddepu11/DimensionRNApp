36b285a62dcab4bc8fb7fb921e19d3255354c38e
deepanshu tiwari <ddepu11@gmail.com>
2024-07-14 17:42:53 +0530

    feat: add support for keybboard avoid view

--- src/Components/ScreenWrapper.tsx
+++ src/Components/ScreenWrapper.tsx
@@ -2,6 +2,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import React, { FC, PropsWithChildren } from "react";
 import { Keyboard, Pressable } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const ScreenWrapper: FC<PropsWithChildren> = ({ children }) => {
   // When user presses out side of any input box close the keyboard
@@ -10,9 +11,11 @@ const ScreenWrapper: FC<PropsWithChildren> = ({ children }) => {
   return (
     <SafeAreaView className="flex-1 px-5">
       <Pressable className="flex-1" onPress={closeKeyboard}>
-        <StatusBar style="auto" />
+        <KeyboardAwareScrollView extraHeight={120} enableOnAndroid>
+          <StatusBar style="auto" />
 
-        {children}
+          {children}
+        </KeyboardAwareScrollView>
       </Pressable>
     </SafeAreaView>
   );
