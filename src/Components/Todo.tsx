8c18ee2085a1a96a6f2cd441e41da90d6d529262
deepanshu tiwari <ddepu11@gmail.com>
2024-07-15 10:33:17 +0530

    feat: add new check box with better style

--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from "react";
 import { Pressable, View } from "react-native";
-import { Checkbox, Text } from "react-native-paper";
+import { Text } from "react-native-paper";
 import { Ionicons } from "@expo/vector-icons";
+import CheckBox from "@react-native-community/checkbox";
+
 import { TodoWithID } from "../../types";
 
 type Props = {
@@ -11,21 +13,30 @@ type Props = {
   handleDeleteTodo: (todo: TodoWithID) => void;
 };
 
-const Todo: FC<Props> = ({ index, todo, handleComplete, handleDeleteTodo }) => {
-  const { completed, content, order } = todo;
-  console.log("____todo", todo);
+const Todo: FC<Props> = ({ todo, handleComplete, handleDeleteTodo }) => {
+  const { completed, content } = todo;
+  // console.log("____todo", todo);
 
   const completeTodo = () => handleComplete({ ...todo, completed: !completed });
   const deleteTodod = () => handleDeleteTodo({ ...todo });
 
   return (
-    <View className="mb-4 flex-row align-middle justify-between rounded-lg bg-slate-500 bottom-2">
-      <View className={"flex-row"}>
-        <Checkbox
-          status={completed ? "checked" : "unchecked"}
-          onPress={completeTodo}
+    <View className="mb-5 flex-row  justify-between rounded-lg">
+      <View
+        className={"flex-row pl-1 justify-start"}
+        style={{ alignItems: "center" }}
+      >
+        <CheckBox
+          disabled={false}
+          value={completed}
+          onValueChange={completeTodo}
+          boxType="square"
+          style={{ width: 20, height: 20 }}
         />
-        <Text className="text-center text-2xl ml-2">{content}</Text>
+
+        <Text className="text-l ml-2" style={{ width: "86%" }}>
+          The problem is with the Fast refresh if you want to
+        </Text>
       </View>
 
       <Pressable onPress={deleteTodod} className="justify-center">
