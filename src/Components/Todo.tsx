00a56077c49ea9873ad298f9b6d3e71b19d0f6ac
deepanshu tiwari <ddepu11@gmail.com>
2024-07-15 12:24:06 +0530

    fix: event propogation

--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -1,5 +1,10 @@
-import React, { FC } from "react";
-import { Pressable, View } from "react-native";
+import React, { FC, useRef } from "react";
+import {
+  GestureResponderEvent,
+  Pressable,
+  StyleSheet,
+  View,
+} from "react-native";
 import { Text } from "react-native-paper";
 import { Ionicons } from "@expo/vector-icons";
 import CheckBox from "@react-native-community/checkbox";
@@ -11,24 +16,45 @@ type Props = {
   todo: TodoWithID;
   handleComplete: (todo: TodoWithID) => void;
   handleDeleteTodo: (todo: TodoWithID) => void;
+  openModal: () => void;
 };
 
-const Todo: FC<Props> = ({ todo, handleComplete, handleDeleteTodo }) => {
+const Todo: FC<Props> = ({
+  todo,
+  handleComplete,
+  handleDeleteTodo,
+  openModal,
+}) => {
   const { completed, content } = todo;
-  // console.log("____todo", todo);
 
   const completeTodo = () => handleComplete({ ...todo, completed: !completed });
   const deleteTodod = () => handleDeleteTodo({ ...todo });
 
+  const flag = useRef(false);
+
+  const checkBoxOnTouchStart = (e: GestureResponderEvent) => {
+    flag.current = false;
+  };
+
+  const checkBoxOnTouchEnd = (e: GestureResponderEvent) => {
+    flag.current = true;
+  };
+
   return (
-    <View className="mb-5 flex-row  justify-between rounded-lg">
-      <View className={"flex-row pl-1 justify-start items-center"}>
+    <Pressable
+      className="mb-4 flex-row justify-between rounded-2xl border-2 border-gray-200 bg-white p-3"
+      style={styles.boxShadow}
+      onPress={flag.current ? openModal : null}
+    >
+      <View className={"flex-row justify-start items-center"}>
         <CheckBox
           disabled={false}
           value={completed}
+          onTouchStart={checkBoxOnTouchStart}
+          onTouchEnd={checkBoxOnTouchEnd}
           onValueChange={completeTodo}
           boxType="square"
-          style={{ width: 20, height: 20 }}
+          style={{ width: 20, height: 20, zIndex: 9999 }}
         />
 
         <Text
@@ -45,8 +71,21 @@ const Todo: FC<Props> = ({ todo, handleComplete, handleDeleteTodo }) => {
       <Pressable onPress={deleteTodod} className="items-center">
         <Ionicons name="trash-bin-sharp" size={18} color="red" />
       </Pressable>
-    </View>
+    </Pressable>
   );
 };
+
+const styles = StyleSheet.create({
+  boxShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
+});
 
 export default Todo;
